477a6d08091a7d9a0929c4d80db3de70a378bf98
afjaramillo <makuandres@utp.edu.co>
2017-08-14 23:17:01 -0500

    estructura con las funciones de ciclos

--- Proyecto/src/src/estructurasdedatos/EstructurasDeDatos.java
+++ Proyecto/src/src/estructurasdedatos/EstructurasDeDatos.java
@@ -5,6 +5,8 @@
  */
 package estructurasdedatos;
 
+import estructurasdedatos.cap1.complejidad.Ciclos;
+
 /**
  *
  * @author tusk
@@ -15,7 +17,17 @@ public class EstructurasDeDatos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ciclos ejerciciosciclos = new Ciclos();
+        
+        ejerciciosciclos.ejecutarejercicio1();
+        ejerciciosciclos.ejecutarejercicio2();
+        ejerciciosciclos.ejecutarejercicio3();
+        ejerciciosciclos.ejecutarejercicio4();
+        
+        
+        
     }
+    
+  
     
 }
