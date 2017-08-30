a7227972fa703d781df734fc46c252aae94450dd
afjaramillo <makuandres@utp.edu.co>
2017-08-29 21:15:46 -0500

    figurageometrica y persona

---
+++ Proyecto/src/src/estructurasdedatos/cap1/tiposdedatos/figurasgeometricas.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructurasdedatos.cap1.tiposdedatos;
+
+/**
+ *
+ * @author Maku
+ */
+public class figurasgeometricas {
+    
+    private  String codigo;
+   private  String tipo;
+   private  String nombre;
+  
+  
+  
+  public void getCodigo(String codigo){
+    return codigo;
+  }
+  
+  public String setCodigo(){
+    this.codigo = codigo;
+  }
+   
+  public void getTipo(String tipo)){
+    return tipo;
+  }
+  public String setTipo({
+    this.tipo = tipo;
+  }
+  
+  public void String getNombre(String nombre){
+    this.nombre = nombre;
+  }
+   public String setNombre(){
+    this.nombre = nombre;
+}
+    
+}
