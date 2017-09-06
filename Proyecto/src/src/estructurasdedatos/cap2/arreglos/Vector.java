4fd608e0ba536e1b43108505b19edb04659b09a2
afjaramillo <makuandres@utp.edu.co>
2017-09-05 21:31:36 -0500

    estructura vector

--- Proyecto/src/src/estructurasdedatos/cap2/arreglos/Vector.java
+++ Proyecto/src/src/estructurasdedatos/cap2/arreglos/Vector.java
@@ -11,4 +11,64 @@ package estructurasdedatos.cap2.arreglos;
  */
 public class Vector {
     
+    private int tamaño;
+    private String[] vector;
+
+
+    public void crearVector(int n) {        
+        this.tamaño = n; 
+        this.vector = new String[this.tamaño]; 
+    }
+    
+    public void settam(int t) {
+        this.tamaño = t;
+    }
+    
+    public int gettam() {
+        return tamaño;
+    }   
+    
+    public boolean Eliminar(int t) {  
+        this.vector = null;
+        return true;
+    }
+    
+    public void Insertar(String n, int pos) { 
+        vector[pos] = n;                     
+    }
+    
+    public String[] listar() {
+
+        for (int i = 0; i < tamaño; i++) {     
+            System.out.println(vector[i]);  
+        }
+        return vector;
+    }
+    
+    public void buscarDato(String dato) {  
+        int i = 0;
+
+        while (i < tamaño) {                   
+            if (vector[i].equals(dato)) {   
+                System.out.println("el dato solicitado esta en la posicion: " + i);  
+            }
+            i++;
+        }
+    } 
+    
+    public void actualizar (int pos, String dato){   
+        if(pos < tamaño){                             
+            System.out.println("la posicion indicada no existe"); 
+        }else {
+            vector[pos]= dato;                       
+        }
+    } 
+    
+    public void borrarposicion (int pos){            
+        vector[pos]= null;                           
+}
+    
+    
+    
+    
 }
