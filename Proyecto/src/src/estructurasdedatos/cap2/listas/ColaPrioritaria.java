11f6a026f9409c36f8e381e23b33eb8a0e40f0c6
afjaramillo <makuandres@utp.edu.co>
2017-10-08 18:51:59 -0500

    cola prioritaria

--- Proyecto/src/src/estructurasdedatos/cap2/listas/ColaPrioritaria.java
+++ Proyecto/src/src/estructurasdedatos/cap2/listas/ColaPrioritaria.java
@@ -10,5 +10,55 @@ package estructurasdedatos.cap2.listas;
  * @author tusk
  */
 public class ColaPrioritaria {
+   
+    private Cola colanormal;
+    private Cola colaempresarial;
+    private Cola colaprioritaria;
+
+   
+    public void crearcolas() {
+        this.colanormal = new Cola();
+        this.colaempresarial = new Cola();
+        this.colaprioritaria = new Cola();
+    }
     
+    
+    public void hacercola(int prioridad, int cedula) {
+            if (prioridad == 0) {
+            if (colanormal.size() < colaempresarial.size()) {
+                this.colanormal.encolar(cedula);             
+            } else {
+                this.colaempresarial.encolar(cedula);
+            }
+        } else {
+            if (prioridad ==1){
+                this.colaprioritaria.encolar(cedula);
+             }
+        }
+
+    }
+
+    
+    public void listar() {
+        System.out.println("la cola Normal es: ");
+        colanormal.listar();
+        System.out.println("\n");
+        System.out.println("la cola Empresarial es: ");
+        colaempresarial.listar();
+        System.out.println("\n");
+        System.out.println("la cola Prioritaria es: ");
+        colaprioritaria.listar();
+
+    }
+    
+    
+    public void atender() { 
+        for (int i = 0; i <= colaprioritaria.size(); i++) {
+            colaprioritaria.desencolar();}
+        if (colaprioritaria.size() == 0) {
+            colanormal.desencolar();
+            colaempresarial.desencolar();
+            
+        }
+    }
 }
