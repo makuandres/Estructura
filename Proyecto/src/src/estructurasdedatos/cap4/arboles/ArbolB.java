cec65ad90ab3456ffc114c2c28c5e295514646cd
afjaramillo <makuandres@utp.edu.co>
2017-10-22 20:49:45 -0500

    arbol binario

--- Proyecto/src/src/estructurasdedatos/cap4/arboles/ArbolB.java
+++ Proyecto/src/src/estructurasdedatos/cap4/arboles/ArbolB.java
@@ -11,4 +11,112 @@ package estructurasdedatos.cap4.arboles;
  */
 public class ArbolB{
 
-}
+    private NodoArbol raiz;
+
+        public boolean vacio() {
+            return raiz == null;
+        }
+
+        
+    public void Insertar(int nodo) {
+            if (vacio()) {
+                NodoArbol nuevonodo = new NodoArbol();
+                nuevonodo.dato = nodo;
+                nuevonodo.hijoderecho= new ArbolB();
+                nuevonodo.hijoizquierdo= new ArbolB();
+                raiz=nuevonodo;
+           }else{
+                if(nodo > raiz.dato){
+                    raiz.hijoderecho.Insertar(nodo);
+                }
+                if(nodo < raiz.dato){
+                    raiz.hijoizquierdo.Insertar(nodo);
+                }
+            }
+        }       
+       
+        
+    public ArbolB Buscar(int nodo){
+            ArbolB subarbol = null;
+            if(!vacio()){
+                if(nodo == raiz.dato){
+                    return this;
+                }else{
+                    if(nodo < raiz.dato){
+                        subarbol=raiz.hijoizquierdo.Buscar(nodo);
+                    }else{
+                        subarbol=raiz.hijoderecho.Buscar(nodo);
+                    }
+                }
+            }
+            
+            return subarbol;
+        }
+       
+    
+    public int buscarMin() {
+        ArbolB arbolActual = this;
+        while( !arbolActual.raiz.hijoizquierdo.vacio() ) {
+            arbolActual = arbolActual.raiz.hijoizquierdo;
+        }
+        int devuelvo= arbolActual.raiz.dato;
+        arbolActual.raiz=null;
+        return devuelvo;
+
+    }
+ 
+
+    public int buscarMan() {
+        ArbolB arbolActual = this;
+        while( !arbolActual.raiz.hijoderecho.vacio() ) {
+            arbolActual = arbolActual.raiz.hijoderecho;
+        }
+        int devuelvo= arbolActual.raiz.dato;
+            arbolActual.raiz=null;
+        return devuelvo;
+    }
+     
+    
+    public int AlturaArbol(){
+            if(vacio()){
+                return 0;
+            }else{
+                int hijoizquierdo=raiz.hijoizquierdo.AlturaArbol();
+                int hijoderecho=raiz.hijoderecho.AlturaArbol();
+                return (1+Math.max(hijoizquierdo, hijoderecho));
+            }
+        }
+              
+        
+    public boolean hoja(){
+            boolean hoja=false;
+            if(raiz.hijoizquierdo.vacio() && raiz.hijoderecho.vacio()){
+                hoja = true;
+            }
+            return hoja;
+        }
+    
+    
+    public void borrar(int a) {
+        ArbolB eliminar = Buscar(a);
+        if (!eliminar.vacio()) {
+            if (eliminar.hoja()) {
+                eliminar.raiz = null;
+            }
+            else {
+                if (!eliminar.raiz.hijoizquierdo.vacio() && !eliminar.raiz.hijoderecho.vacio()) {
+                    eliminar.raiz.dato = eliminar.raiz.hijoderecho.buscarMin();
+                }
+                else {
+                    if (eliminar.raiz.hijoizquierdo.vacio()) {
+                        eliminar.raiz = eliminar.raiz.hijoderecho.raiz;
+                    }else{
+                        eliminar.raiz = eliminar.raiz.hijoizquierdo.raiz;
+                    }
+                }
+            }
+        }
+    }
+
+    
+    }
---
+++ Proyecto/src/src/estructurasdedatos/cap4/arboles/NodoArbol.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estructurasdedatos.cap4.arboles;
+
+/**
+ *
+ * @author Maku
+ */
+public class NodoArbol {
+    
+        public ArbolB hijoderecho;
+        public ArbolB hijoizquierdo;
+        public int dato;
+
+        public void NodoArbol() {
+            this.hijoderecho = null;
+            this.hijoizquierdo = null;
+            this.dato = 0;
+        }
+}
